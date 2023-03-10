1c3270d5dbe739a142f2fcca7cfb548878389d7c
EgorPestov <egorpestov2012@yandex.ru>
2023-03-10 16:46:44 +0400

    Пока все работает, приступаю к комментам

--- js/rendering-mini.js
+++ js/rendering-mini.js
@@ -4,14 +4,13 @@ import {createObjects} from './data.js';
 
 const picturesContainer = document.querySelector('.pictures');
 const objects = createObjects();
-
 const renderPhotos = () => {
   const fragment = document.createDocumentFragment();
   const pictureTemplate = document.querySelector('#picture').content;
 
   for (let i = 0; i < objects.length; i++) {
     const picture = pictureTemplate.cloneNode(true);
-    picture.querySelector('a').setAttribute('about', objects[i].id);
+    picture.querySelector('a').dataset.id = objects[i].id;
     picture.querySelector('.picture__img').src = objects[i].url;
     picture.querySelector('.picture__likes').textContent = objects[i].likes;
     picture.querySelector('.picture__comments').textContent = objects[i].comments.length;
@@ -23,4 +22,4 @@ const renderPhotos = () => {
 
 renderPhotos();
 
-export {picturesContainer};
+export {objects, picturesContainer};
