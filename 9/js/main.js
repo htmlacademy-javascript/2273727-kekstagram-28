0c55e1638587c02c7bbd6983618ebd1646994928
Keks <keks@htmlacademy.ru>
2023-03-18 18:27:06 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/main.js
@@ -0,0 +1,5 @@
+import './rendering-mini.js';
+import './rendering-full.js';
+import './upload.js';
+import './filter.js';
+import './redactor.js';
---
+++ 9/js/rendering-full.js
@@ -0,0 +1,49 @@
+// логика отрисовщика большого фото и комментариев
+
+import { objects, picturesContainer } from './rendering-mini.js';
+import { renderComments } from './rendering-comments.js';
+import { onLoaderClick } from './change-comments.js';
+import { isEscKeydown } from './util.js';
+
+const bigPicture = document.querySelector('.big-picture');
+
+function onMiniatureClick (evt) {
+  if (evt.target.closest('.picture')) {
+    evt.preventDefault();
+    const id = evt.target.closest('a').dataset.id;
+    bigPicture.classList.remove('hidden');
+    bigPicture.querySelector('.big-picture__img').querySelector('img').src = objects[id].url;
+    bigPicture.querySelector('.big-picture__img').querySelector('img').alt = objects[id].description;
+    bigPicture.querySelector('.social__caption').textContent = objects[id].description;
+    bigPicture.querySelector('.likes-count').textContent = objects[id].likes;
+    bigPicture.querySelector('.comments-count').textContent = objects[id].comments.length;
+    renderComments(id);
+
+    const commentContainer = bigPicture.querySelector('.social__comments');
+    const commentsLength = commentContainer.children.length;
+    const commentsLoader = bigPicture.querySelector('.comments-loader');
+    commentsLoader.classList.toggle('hidden', commentsLength <= 5);
+    bigPicture.querySelector('.visible-comments-count').textContent = (commentsLength > 5) ? 5 : commentsLength;
+
+    commentsLoader.addEventListener('click', onLoaderClick);
+    document.querySelector('body').classList.add('modal-open');
+  }
+}
+
+picturesContainer.addEventListener('click', onMiniatureClick);
+
+function onCloseClick () {
+  bigPicture.classList.add('hidden');
+  document.querySelector('body').classList.remove('modal-open');
+  bigPicture.querySelector('.comments-loader').removeEventListener('click', onLoaderClick);
+}
+
+bigPicture.querySelector('.big-picture__cancel').addEventListener('click', onCloseClick);
+
+document.addEventListener('keydown', (evt) => {
+  if (isEscKeydown(evt)) {
+    bigPicture.classList.add('hidden');
+    bigPicture.querySelector('.comments-loader').removeEventListener('click', onLoaderClick);
+    document.querySelector('body').classList.remove('modal-open');
+  }
+});
