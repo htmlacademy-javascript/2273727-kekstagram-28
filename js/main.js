2ffc280db1d4376371cd6c5e2ce13eb927de063d
EgorPestov <egorpestov2012@yandex.ru>
2023-03-28 11:55:02 +0400

    Добавил отображение выбранного пользователем фото

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ import { closeForm, setFormSubmit } from './upload.js';
 import './rendering-mini.js';
 import './rendering-full.js';
 import './redactor.js';
+import './photo.js';
 
 getData();
 setFormSubmit(closeForm);
