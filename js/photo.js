bd09daed025d281350cea14dccb25a738173b4f6
EgorPestov <egorpestov2012@yandex.ru>
2023-03-30 11:38:25 +0400

    Исправил отображение миниатюр эффектов

--- js/photo.js
+++ js/photo.js
@@ -3,6 +3,7 @@
 const FILE_TYPES = ['jpg', 'jpeg', 'png'];
 const fileChooser = document.querySelector('.img-upload__input');
 const preview = document.querySelector('.img-upload__preview img');
+const miniatureList = document.querySelectorAll('.effects__preview');
 
 fileChooser.addEventListener('change', () => {
   const file = fileChooser.files[0];
@@ -12,5 +13,8 @@ fileChooser.addEventListener('change', () => {
 
   if (matches) {
     preview.src = URL.createObjectURL(file);
+    miniatureList.forEach((miniature) => {
+      miniature.style.backgroundImage = `url(${URL.createObjectURL(file)})`;
+    });
   }
 });
